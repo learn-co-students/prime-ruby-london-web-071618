517bec7136fee734654b0eb947f9d6d3e33264d8
ritz-w <ritz.wu@outlook.com>
2018-07-03 16:50:09 +0100

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
+def prime?(int)
+  range = (2..50).to_a
+  if range.include?(int)
+    int_index = range.find_index(int)
+    range.delete_at(int_index)
+  end
+  new_range = []
+  range.each do |other_number|
+      new_range.push(int % other_number)
+  end
+  if new_range.include?(0) || int <= 1
+    return false
+  else
+    return true
+  end
+end
 # Add  code here!
